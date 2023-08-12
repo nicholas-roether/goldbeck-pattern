d884fe2d22d01d2082edd96f9d3ee8606aa908c1
Nicholas Roether <nicholas.roether@t-online.de>
2023-08-12 18:45:40 +0200

    Fix tailwind config formatting

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,64 +1,64 @@
 import { createThemes } from "tw-colors";
 
 const COLORS = {
-  carbon: "#2b323a",
-  blue: "#3ebaec",
-  white: "#fff",
-  gold: "#c1a368",
-  aluminium: "#b8bbbe",
+	carbon: "#2b323a",
+	blue: "#3ebaec",
+	white: "#fff",
+	gold: "#c1a368",
+	aluminium: "#b8bbbe",
 };
 
 export default {
-  content: {
-    files: ["*.html", "./src/**/*.rs"],
-  },
-  theme: {
-    fontFamily: {
-      sans: ["Montserrat", "sans-serif"],
-    },
-  },
-  plugins: [
-    createThemes({
-      bubbles: {
-        background: COLORS.white,
-        backgroundText: COLORS.carbon,
-        primary: COLORS.carbon,
-        primaryText: COLORS.white,
-        secondary: COLORS.blue,
-		secondaryText: COLORS.carbon,
-	    misc: COLORS.aluminium,
-	    highlight: COLORS.gold
-      },
-      icy: {
-        background: COLORS.carbon,
-        backgroundText: COLORS.white,
-        primary: COLORS.white,
-        primaryText: COLORS.carbon,
-        secondary: COLORS.blue,
-		secondaryText: COLORS.carbon,
-	    misc: COLORS.aluminium,
-	    highlight: COLORS.gold
-      },
-	  glisten: {
-	    background: COLORS.white,
-	    backgroundText: COLORS.carbon,
-	    primary: COLORS.carbon,
-	    primaryText: COLORS.white,
-	    secondary: COLORS.gold,
-		secondaryText: COLORS.carbon,
-	    misc: COLORS.aluminium,
-	    highlight: COLORS.blue
-	  },
-	  noble: {
-	    background: COLORS.carbon,
-		backgroundText: COLORS.white,
-		primary: COLORS.blue,
-		primaryText: COLORS.carbon,
-		secondary: COLORS.gold,
-		secondaryText: COLORS.carbon,
-		misc: COLORS.aluminium,
-		highlight: COLORS.white
-	  }
-    }),
-  ],
+	content: {
+		files: ["*.html", "./src/**/*.rs"],
+	},
+	theme: {
+		fontFamily: {
+			sans: ["Montserrat", "sans-serif"],
+		},
+	},
+	plugins: [
+		createThemes({
+			bubbles: {
+				background: COLORS.white,
+				backgroundText: COLORS.carbon,
+				primary: COLORS.carbon,
+				primaryText: COLORS.white,
+				secondary: COLORS.blue,
+				secondaryText: COLORS.carbon,
+				misc: COLORS.aluminium,
+				highlight: COLORS.gold
+			},
+			icy: {
+				background: COLORS.carbon,
+				backgroundText: COLORS.white,
+				primary: COLORS.white,
+				primaryText: COLORS.carbon,
+				secondary: COLORS.blue,
+				secondaryText: COLORS.carbon,
+				misc: COLORS.aluminium,
+				highlight: COLORS.gold
+			},
+			glisten: {
+				background: COLORS.white,
+				backgroundText: COLORS.carbon,
+				primary: COLORS.carbon,
+				primaryText: COLORS.white,
+				secondary: COLORS.gold,
+				secondaryText: COLORS.carbon,
+				misc: COLORS.aluminium,
+				highlight: COLORS.blue
+			},
+			noble: {
+				background: COLORS.carbon,
+				backgroundText: COLORS.white,
+				primary: COLORS.blue,
+				primaryText: COLORS.carbon,
+				secondary: COLORS.gold,
+				secondaryText: COLORS.carbon,
+				misc: COLORS.aluminium,
+				highlight: COLORS.white
+			}
+		})
+	],
 };

7e1938b57d0194c40b8fe2f8d0d5ea5858a433c9
Nicholas Roether <nicholas.roether@t-online.de>
2023-08-12 16:49:44 +0200

    Implement brush controls

--- src/components/editor.rs
+++ src/components/editor.rs
@@ -204,33 +204,48 @@ pub fn Canvas(
 // }
 
 #[component]
+fn BrushButton(
+	cx: Scope,
+	name: &'static str,
+	icon: &'static str,
+	color: TileColor,
+	brush: RwSignal<TileColor>
+) -> impl IntoView {
+	view! { cx,
+		<button
+			role="radio"
+			aria-label=name
+			class=move || String::new()
+				+ "inline-flex p-2 "
+				+ match color {
+					TileColor::Primary => "bg-primary text-primaryText ",
+					TileColor::Secondary => "bg-secondary text-secondaryText ",
+					TileColor::None => "bg-background text-backgroundText "
+				}
+				+ if brush() == color { "relative z-100 outline outline-3 outline-highlight " } else { " " }
+			on:click=move |_| brush.set(color)
+		>
+			<box-icon name=icon size="md" color="currentColor" />
+		</button>
+	}
+}
+
+#[component]
+fn BrushControls(cx: Scope, brush: RwSignal<TileColor>) -> impl IntoView {
+	view! { cx,
+		<div role="radiogroup" aria-label="Werkzeug auswählen" class="flex w-full max-w-sm border-2 border-misc">
+			<BrushButton name="Pinsel 1" icon="brush" color=TileColor::Primary brush />
+			<BrushButton name="Pinsel 2" icon="brush" color=TileColor::Secondary brush />
+			<BrushButton name="Radiergummi" icon="eraser" color=TileColor::None brush />
+		</div>
+	}
+}
+
+#[component]
 fn Controls(cx: Scope, brush: RwSignal<TileColor>) -> impl IntoView {
 	view! { cx,
-		<div role="radiogroup" aria-label="Werkzeug auswählen" class="p-6 flex w-full max-w-sm">
-			<button
-				role="radio"
-				aria-label="Pinsel 1"
-				class="bg-primary text-primaryText"
-				on:click=move |_| brush.set(TileColor::Primary)
-			>
-				<box-icon name="brush" size="md" color="currentColor" />
-			</button>
-			<button
-				role="radio"
-				aria-label="Pinsel 1"
-				class="bg-secondary text-secondaryText"
-				on:click=move |_| brush.set(TileColor::Secondary)
-			>
-				<box-icon name="brush" size="md" color="currentColor" />
-			</button>
-			<button
-				role="radio"
-				aria-label="Pinsel 1"
-				class="bg-background text-backgroundText"
-				on:click=move |_| brush.set(TileColor::None)
-			>
-				<box-icon name="eraser" size="md" color="currentColor" />
-			</button>
+		<div class="p-6">
+			<BrushControls brush />
 		</div>
 	}
 }
